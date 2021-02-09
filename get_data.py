692611584f2f4965856c9da391260c4afce0b803
ax0170 <alexandr.sh.0107@mail.ru>
2021-02-09 10:24:28 +0300

    add

--- get_data.py
+++ get_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import wfdb
 from wfdb import processing
 import numpy as np
+import tensorflow as tf
 
 
 DATA_DIR = 'data/lobachevsky-university-electrocardiography-database-1.0.1'
@@ -12,6 +13,16 @@ df['Age'].replace('>89', 90, inplace=True)
 df['Age'] = df['Age'].apply(pd.to_numeric)
 
 data_10_to_21 = df.loc[(16 < df['Age']) & (df['Age'] < 21)]
+
+DIAGNOSES = np.unique(df['Rhythms'])
+print('Count of DIAGNOSES:', len(DIAGNOSES))
+
+
+def get_class_of_diagnose(diagnose):
+    global DIAGNOSES
+    diagnose = diagnose.replace('Rhythm: ', '').replace('.', '')
+    index = np.where(DIAGNOSES == diagnose.replace('Rhythm: ', '')) or -1
+    return index[0][0]
 
 
 def calculate_heart_rate(fs, r_peaks=None, diff=None):
@@ -36,23 +47,29 @@ def feature_selection(signal, fs, r_peaks_ix):
     HR = calculate_heart_rate(fields['fs'], r_peaks=r_peaks_ix)
     return {'RPM': RR_middle, 'HR': HR}
 
+def main():
+    for i in range(0, len(data_10_to_21)):
+        fid = data_10_to_21.iloc[i]['ID']
 
-for i in range(0, len(data_10_to_21)):
-    fid = data_10_to_21.iloc[i]['ID']
+        signal, fields = wfdb.rdsamp(f'{DATA_DIR}/data/{fid}')
+        fs = fields['fs']
+        r_peaks_ix = processing.gqrs_detect(signal[:, 0], fs=fs, threshold=1.0)
 
-    signal, fields = wfdb.rdsamp(f'{DATA_DIR}/data/{fid}')
-    fs = fields['fs']
-    r_peaks_ix = processing.gqrs_detect(signal[:, 0], fs=fs, threshold=1.0)
+        print(feature_selection(signal, fs, r_peaks_ix), get_class_of_diagnose(fields['comments'][3]))
 
-    print(feature_selection(signal, fs, r_peaks_ix), fields['comments'])
-
-    # # Проходимся по данным между пиками R
-    # for i in range(0, len(r_peaks_ix), 2):
-    #     data = signal[r_peaks_ix[i]:r_peaks_ix[i+1]]
-    #     data = data.T[0]
-    #     print(len(data))
-    #
-    #     # print("xa, ya = {}, {}".format(maximums[0].tolist(), data[maximums].tolist()), end='\n\n')
-    #     break
-    # break
-
+        # # Проходимся по данным между пиками R
+        # for i in range(0, len(r_peaks_ix), 2):
+        #     data = signal[r_peaks_ix[i]:r_peaks_ix[i+1]]
+        #     data = data.T[0]
+        #     print(len(data))
+        #
+        #     # print("xa, ya = {}, {}".format(maximums[0].tolist(), data[maximums].tolist()), end='\n\n')
+        #     break
+        # break
+#
+#
+# model = tf.keras.models.Sequential([
+#   tf.keras.layers.Flatten(input_shape=(2,)),
+#   tf.keras.layers.Dense(10, activation='relu'),
+#   tf.keras.layers.Dense(10)
+# ])
