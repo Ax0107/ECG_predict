580c0a0dc4f9059a79440208909b0a1adadc15f9
ax0170 <alexandr.sh.0107@mail.ru>
2021-04-20 03:26:26 +0300

    Обновление api & модели

--- api.py
+++ api.py
@@ -3,16 +3,16 @@ import pandas as pd
 import wfdb
 from wfdb import processing
 import json
+from main import start, get_trained_model
+from random import randint, choice
 
 DATA_DIR = 'data/lobachevsky-university-electrocardiography-database-1.0.1'
 
-# Preprocessing
-df = pd.read_csv(f'{DATA_DIR}/ludb.csv')
-df['Age'] = df['Age'].apply(lambda x: x.replace('\n', ''))
-df['Age'].replace('>89', 90, inplace=True)
-df['Age'] = df['Age'].apply(pd.to_numeric)
+df = start()
 
 app = Flask(__name__)
+
+MODEL = None
 
 
 def get_data(age_start, age_stop, limit=1):
@@ -26,11 +26,38 @@ def get_data(age_start, age_stop, limit=1):
 
     for i in range(0, limit):
         try:
-            fid = data_for_age_range.iloc[i]['ID']
+            pdf = data_for_age_range.iloc[i]
+            fid = pdf['ID']
 
             signal, fields = wfdb.rdsamp(f'{DATA_DIR}/data/{fid}')
             qrs_locs = processing.gqrs_detect(signal[:, 0], fs=fields['fs'])
-            result['data'].append({'ecg_data': signal.tolist(), 'qrs_ix': qrs_locs.tolist(), 'fields': fields})
+            # result['data'].append({'ecg_data': signal.tolist(), 'qrs_ix': qrs_locs.tolist(), 'fields': fields})
+
+            patient_data = {
+                    'avatar': f'require("~/assets/images/users/avatar-{randint(1, 4)}.jpg")',
+                    'name': choice(["Смирнов", "Кузнецов", "Попов", "Васильев", "Петров", "Соколов", "Михайлов",
+                                    "Фролов", "Журавлёв", "Николаев", "Крылов", "Максимов", "Сидоров", "Осипов", "Белоусов",
+                                    "Федотов", "Дорофеев", "Егоров", "Матвеев", "Бобров", "Дмитриев","Калинин", 'Петров', "Васильев", "Тылык", "Иванов", "Пушкин", "Лермонтов", "Данилин", "Ванилин", "Герц", "Тесла", "Улец", "Кершин", "Ким"])+' '+choice(['И.', "А.", "В.", "Д.", "К."])+choice(['И.', "А.", "В.", "Д."]),
+                    'age': str(pdf['Age']),
+                    'id': str(pdf['ID']),
+                    'diagnose': pdf['Rhythms'],
+                    'state': 'Отклонение от нормы' if pdf['Rhythms'] != 'Sinus rhythm' else 'В норме',
+                    'last_visit': f"{randint(1, 31)}/{randint(1, 12)}/2020",
+                    'last_data_update': f"{randint(1, 31)}/{randint(1, 12)}/2021",
+                    'tmt_online': choice(['Online', 'Offline']),
+                    'link': '/dashboard/diagnosis/index2' if pdf['Rhythms'] != 'Sinus rhythm' else '/dashboard/diagnosis',
+                  },
+            print('{', end='')
+            for key, value in patient_data[0].items():
+
+                if key == 'avatar':
+                    print(f"{key}: {value}", end=', ')
+
+                else:
+                    print(f"{key}: '{value}'", end=', ')
+            print('},')
+            result['data'].append(patient_data)
+
         except Exception as e:
             print('Error (getting ecg data): {}'.format(e))
     print(result)
@@ -38,18 +65,35 @@ def get_data(age_start, age_stop, limit=1):
     return result
 
 
-@app.route('/', methods=['GET'])
-def api():
-
+@app.route('/data', methods=['GET'])
+def data():
     data = '{}'
     if request.method == 'GET':
-        age_start = request.args.get('age_start', 21)
-        age_stop = request.args.get('age_stop', 35)
-        limit = request.args.get('limit', 1)
+        age_start = request.args.get('age_start', 0)
+        age_stop = request.args.get('age_stop', 100)
+        limit = request.args.get('limit', 200)
         data = get_data(age_start, age_stop, limit)
 
     return data
 
 
+@app.route('/predict_for_id', methods=['GET'])
+def predict():
+    data = '{}'
+    if request.method == 'GET':
+        pid = int(request.args.get('id', 0))
+        data = df.iloc[pid]
+        if MODEL is not None:
+            prediction = MODEL.predict([data[['RPM', "HR"]]])
+            prediction_text = f'PREDICTION: {"Пациент здоров" if prediction else "Пациент нездоров"}\nACTUAL: {"Пациент здоров" if data["is_healthy"] else "Пациент нездоров"}'
+            return prediction_text
+
+
+def get_model():
+    global MODEL
+    MODEL = get_trained_model(df)
+
+
 if __name__ == "__main__":
+    get_model()
     app.run('localhost', 9000)
--- main.py
+++ main.py
@@ -1,25 +1,115 @@
-from flask import Flask, request
-from model import predict_for_ECG
+import pandas as pd
+import wfdb
+from wfdb import processing
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier as Model
+from sklearn.metrics import recall_score, precision_score, f1_score, confusion_matrix
+from os.path import exists
 
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def hello_world():
-
-    text = "Отправьте показатели в GET-запросе. Список показателей: RR, ST, QT, P-tooth, T-tooth"
-    if request.method == 'GET':
-        pr = request.args.get('pr', 0)
-        st = request.args.get('st', 0)
-        qt = request.args.get('qr', 0)
-        p_tooth = request.args.get('p_tooth', 0)
-        t_tooth = request.args.get('t_tooth', 0)
-
-        data = predict_for_ECG(pr=int(pr), st=st, qt=qt, p_tooth=p_tooth, t_tooth=t_tooth)
-        text = 'Result: {}'.format(data)
-
-    return text
+DATA_DIR = 'data/lobachevsky-university-electrocardiography-database-1.0.1'
+DIAGNOSES = None
+PARAMS = ['RPM', 'HR']
 
 
-if __name__ == "__main__":
-    app.run('localhost', 9000)
+def get_class_of_diagnose(diagnose):
+    global DIAGNOSES
+    diagnose = diagnose.replace('Rhythm: ', '').replace('.', '')
+    index = np.where(DIAGNOSES == diagnose.replace('Rhythm: ', '')) or -1
+    return index[0][0]
+
+
+def calculate_heart_rate(fs, r_peaks=None, diff=None):
+    """
+    Рассчитывает пульс в уд./мин.
+    """
+
+    if diff is None and r_peaks is None:
+        return 0
+
+    if r_peaks is not None:
+        # Разница между ударами в ед. индекса
+        diff = np.diff(r_peaks)
+
+    if diff is not None:
+        heart_rate = np.mean(fs / diff * 60)
+        return heart_rate
+
+
+def feature_selection(signal, fs, r_peaks_ix):
+    RR_middle = np.mean(fs / r_peaks_ix)
+    HR = calculate_heart_rate(fs, r_peaks=r_peaks_ix)
+    return {'RPM': RR_middle, 'HR': HR}
+
+
+def process_data(df):
+    global DIAGNOSES
+    df['Age'] = df['Age'].apply(lambda x: x.replace('\n', ''))
+    df['Age'].replace('>89', 90, inplace=True)
+    df['Age'] = df['Age'].apply(pd.to_numeric)
+    df['diagnose_class'] = df.apply(lambda x: get_class_of_diagnose(x['Rhythms']), axis=1)
+    df['is_healthy'] = df.apply(lambda x: 0 if x['diagnose_class'] != 8 else 1, axis=1)
+
+    df['data'] = df.apply(lambda row: get_data_for_patient(row['ID']), axis=1)
+    for param in PARAMS:
+        df[param] = df.apply(lambda row: row['data'][param], axis=1)
+
+    df = df.drop(['data'], axis=1)
+
+    return df
+
+
+def get_data_for_patient(patient):
+    signal, fields = wfdb.rdsamp(f'{DATA_DIR}/data/{patient}')
+    fs = fields['fs']
+    r_peaks_ix = processing.gqrs_detect(signal[:, 0], fs=fs, threshold=1.0)
+
+    features = feature_selection(signal, fs, r_peaks_ix)
+    return features
+
+
+def get_trained_model(df):
+    target_data = df.loc[(10 < df['Age']) & (df['Age'] < 20)]
+    # target_data = df
+
+    print('Got {} samples'.format(target_data.shape[0]))
+
+    train = target_data.iloc[:].replace(np.nan, 0)
+
+    model = Model(n_neighbors=2)
+    model.fit(X=train[['RPM', "HR"]], y=train['is_healthy'])
+    print('Model fit: done')
+    return model
+
+
+def start():
+    global DIAGNOSES
+    if not exists('saved_info_df.csv'):
+        print('Reading data file.')
+        df = pd.read_csv(f'{DATA_DIR}/ludb.csv')
+        DIAGNOSES = np.unique(df['Rhythms'])
+
+        print('Process data:', end='')
+        df = process_data(df)
+        print('OK!')
+        df.to_csv('saved_info_df.csv')
+        print('File saved.')
+    else:
+        df = pd.read_csv('saved_info_df.csv')
+        DIAGNOSES = np.unique(df['Rhythms'])
+
+    print('Data processed.')
+    return df
+
+
+def predict(model, data):
+
+    # TESTING PREDICTION
+
+    prediction = model.predict(data[['RPM', "HR"]])
+    df_result = pd.DataFrame({'pred': prediction, 'actual': data['is_healthy']})
+    print(df_result.head(15))
+
+    return prediction
+
+# if __name__ == "__main__":
+    # main()
